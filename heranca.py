c101526db79f2ce16d8ea57d289a52ebaa4ca04c
marcusmarinhob <marcus.bezerra@virtus.ufcg.edu.br>
2022-09-16 20:48:12 -0300

    Explorando o construtor com 'super()'

--- heranca.py
+++ heranca.py
@@ -10,6 +10,8 @@ class Veiculo:
     def ligar_motor(self):
         print("Ligando o motor...")
 
+    def __str__(self):
+        return self.cor
 
 class Motocicleta(Veiculo):
     pass
@@ -18,7 +20,12 @@ class Carro(Veiculo):
     pass
 
 class Caminhao(Veiculo):
-    pass
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim,' if self.carregado else 'Não'} estou carregado")
 
 
 moto = Motocicleta("vermelho", "ABC-1234", 2)
@@ -27,5 +34,7 @@ moto.ligar_motor()
 carro = Carro("branco", "EFG-5678", 4)
 carro.ligar_motor()
 
-caminhao = Caminhao("azul", "IJK-9012", 8)
+caminhao = Caminhao("azul", "IJK-9012", 8, False)
 caminhao.ligar_motor()
+caminhao.esta_carregado()
+print(caminhao)
