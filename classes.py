887813c9257bcf9f72e0e0c17143020853f034ae
marcusmarinhob <marcus.bezerra@virtus.ufcg.edu.br>
2022-09-16 20:27:08 -0300

    Otimizando a função __str__ com 'list comprehension'

--- classes.py
+++ classes.py
@@ -1,9 +1,10 @@
 class Bicicleta:
-    def __init__(self, cor, modelo, ano, valor):
+    def __init__(self, cor, modelo, ano, valor, aro=18):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+        self.aro = aro
 
     def buzinar(self):
         print("Plim plim...")
@@ -15,9 +16,13 @@ class Bicicleta:
     def correr(self):
         print("Vrummmmmm...")
 
-    def __str__(self):
-        return f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
+    # def __str__(self):
+    #     return f"Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}"
     
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join( [ f'{chave}={valor}' for chave, valor in self.__dict__.items() ] )}"
+        
+
 b1 = Bicicleta("azul", "caloi", 2022, 600)
 b1.buzinar()
 b1.parar()
