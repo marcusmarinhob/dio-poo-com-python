111e018c925c87c060b141d78990fe0111711a9a
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2022-09-25 18:58:34 -0300

    Explorando variáveis de instância e de classe

---
+++ atributos_classe_instancia.py
@@ -0,0 +1,26 @@
+class Estudante:
+    escola = "DIO"
+
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula
+
+    def __str__(self):
+        return f"{self.nome} - {self.matricula} - {self.escola}"
+
+def mostrar_valores(*objs):
+    for obj in objs:
+        print( obj )
+
+
+aluno_1 = Estudante("Gilherme", 1)
+aluno_2 = Estudante("Giovanna", 2)
+
+mostrar_valores(aluno_1, aluno_2)
+
+aluno_1.matricula = 3
+Estudante.escola = "Python"    
+
+aluno_3 = Estudante("Chapie", 3)
+    
+mostrar_valores(aluno_1, aluno_2, aluno_3)
